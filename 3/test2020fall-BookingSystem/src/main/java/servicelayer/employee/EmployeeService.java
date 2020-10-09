ce2179a07d09bfd64e313ebf394b80998f81379a
Claus K <cph-ck83@cphbusiness.dk>
2020-10-09 12:11:33 +0200

    fix

--- 3/test2020fall-BookingSystem/src/main/java/servicelayer/employee/EmployeeService.java
+++ 3/test2020fall-BookingSystem/src/main/java/servicelayer/employee/EmployeeService.java
@@ -4,6 +4,6 @@ import java.util.Date;
 
 public interface EmployeeService
 {
-    int createEmployee(String firstName, String lastName, Date birthdate) throws EmployeeServiceException .;
+    int createEmployee(String firstName, String lastName, Date birthdate) throws EmployeeServiceException;
     Employee getEmployeeById(int employeeId);
 }
