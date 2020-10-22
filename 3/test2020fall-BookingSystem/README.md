edaa5a4bac852d808435c19ff4147931f581aecb
Claus K <cph-ck83@cphbusiness.dk>
2020-10-22 13:01:28 +0200

    Added mutating tests and pit-report.

--- 3/test2020fall-BookingSystem/README.md
+++ 3/test2020fall-BookingSystem/README.md
@@ -1,14 +1,25 @@
-<h1>Booking system.</h1>
-<h2>Requirements:</h2>
+# Booking system.
+
+## Requirements:
+
 A running mysql server exposed at port 3307.
 <br>Root password must be testuser123
 
-Booking-, employee- and customer creation have been tested at unit and integration level.
-<br>
-Invocation of SmsService in accordance with the specified requirements has been tested at unit level.
-<br>
-If a SmsMessage cannot be send due to missing or incomplete phone number, the SmsMessage status field is updated accordingly. The value of this field is also tested against.
+### Setup:
+
+- Clone this repo.
+- Install Docker.
+- Run this command to set up mysql in a Docker container: \
+  <code>docker run -d --rm \
+  --name mysql-test-db \
+  -e MYSQL_ROOT_PASSWORD=testuser123 \
+  -p 3307:3306
+  mysql</code>
+
+### Features:
+
+- Booking-, employee- and customer creation have been tested at unit and integration level.
+- Invocation of SmsService in accordance with the specified requirements has been tested at unit level.
+- If a SmsMessage cannot be sent due to missing or incomplete phone number, the SmsMessage status field is updated accordingly. The value of this field is also tested against.
 
 Enjoy!
- 
-
--- 3/test2020fall-BookingSystem/src/test/java/unit/servicelayer/customer/CreateCustomerTest.java
+++ 3/test2020fall-BookingSystem/src/test/java/unit/servicelayer/customer/CreateCustomerTest.java
@@ -2,10 +2,7 @@ package unit.servicelayer.customer;
 
 import datalayer.customer.CustomerStorage;
 import dto.CustomerCreation;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import servicelayer.customer.CustomerService;
 import servicelayer.customer.CustomerServiceException;
 import servicelayer.customer.CustomerServiceImpl;
@@ -26,8 +23,8 @@ public class CreateCustomerTest {
     private CustomerStorage storageMock;
 
 
-    @BeforeAll
-    public void beforeAll(){
+    @BeforeEach
+    public void beforeEach(){
         storageMock = mock(CustomerStorage.class);
         customerService = new CustomerServiceImpl(storageMock);
     }
@@ -35,10 +32,10 @@ public class CreateCustomerTest {
     @Test
     public void mustCallStorageWhenCreatingCustomer() throws CustomerServiceException, SQLException {
         // Arrange
-        // Act
         var firstName = "a";
         var lastName = "b";
         var birthdate = new Date(123456789l);
+        // Act
         customerService.createCustomer(firstName, lastName, birthdate);
 
         // Assert
@@ -64,12 +61,11 @@ public class CreateCustomerTest {
         // Assert
         // Can be read like: verify that storageMock was called 1 time on the method
         //   'createCustomer' with an argument whose 'firstname' == firstName and
-        //   whose 'lastname' == lastName
+        //   whose 'lastname' == lastName and 'phonono' == phoneno.
         verify(storageMock, times(1))
                 .createCustomer(
                         argThat(x -> x.firstname.equals(firstName) &&
                                 x.lastname.equals(lastName) &&
                                 x.phoneno.equals(phoneNo)));
     }
-
 }
